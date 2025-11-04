4c29313d89b29cf96dfaecc085605411c0b161fb
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-11-04 17:32:52 -0300

    chore: adding structure of repository

--- StatusPirata.java
+++
@@ -1,6 +0,0 @@
-package org.example.enumerators;
-
-public enum StatusPirata {
-    ATIVO, CAPTURADO
-
-}
---
+++ src/main/java/onepiece/enums/NivelPerigo.java
@@ -0,0 +1,5 @@
+package org.example.enumerators.src.main.java.onepiece.enums;
+
+public enum NivelPerigo {
+    D, C, B, A, S
+}
