4cb38c06a5de5a7c529419f4c936091c056c7260
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-11-15 18:47:15 -0300

    feat: complete entities implementation

--- src/main/java/onepiece/entity/Missao.java
+++ src/main/java/onepiece/entity/Missao.java
@@ -1,4 +1,66 @@
-package org.example.enumerators.src.main.java.onepiece.entity;
+package onepiece.entity;
 
+import jakarta.persistence.*;
+import onepiece.enums.NivelPerigo;
+import onepiece.enums.StatusMissao;
+import onepiece.enums.TipoMissao;
+
+@Entity
 public class Missao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NivelPerigo nivelPerigo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoMissao tipoMissao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusMissao statusMissao;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "pirata_id")
+    private Pirata pirata;
+
+    public Long getId() {
+        return id;
+    }
+
+    public NivelPerigo getNivelPerigo() {
+        return nivelPerigo;
+    }
+
+    public void setNivelPerigo(NivelPerigo nivelPerigo) {
+        this.nivelPerigo = nivelPerigo;
+    }
+
+    public TipoMissao getTipoMissao() {
+        return tipoMissao;
+    }
+
+    public void setTipoMissao(TipoMissao tipoMissao) {
+        this.tipoMissao = tipoMissao;
+    }
+
+    public StatusMissao getStatusMissao() {
+        return statusMissao;
+    }
+
+    public void setStatusMissao(StatusMissao statusMissao) {
+        this.statusMissao = statusMissao;
+    }
+
+    public Pirata getPirata() {
+        return pirata;
+    }
+
+    public void setPirata(Pirata pirata) {
+        this.pirata = pirata;
+    }
 }
--- src/main/java/onepiece/entity/Pirata.java
+++ src/main/java/onepiece/entity/Pirata.java
@@ -1,4 +1,72 @@
 package onepiece.entity;
 
+import jakarta.persistence.*;
+import onepiece.enums.Raca;
+import onepiece.enums.StatusPirata;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Pirata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Raca raca;
+
+    @Column(nullable = false)
+    private String tripulacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusPirata status;
+
+    @OneToMany(mappedBy = "pirata", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Missao> missoes = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Raca getRaca() {
+        return raca;
+    }
+
+    public void setRaca(Raca raca) {
+        this.raca = raca;
+    }
+
+    public String getTripulacao() {
+        return tripulacao;
+    }
+
+    public void setTripulacao(String tripulacao) {
+        this.tripulacao = tripulacao;
+    }
+
+    public StatusPirata getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPirata status) {
+        this.status = status;
+    }
+
+    public List<Missao> getMissoes() {
+        return missoes;
+    }
 }
