eca4a8cd481649d19696c0b4dc2dcf578b3722eb
Valentina Scolari <becharascolarivalentina@gmail.com>
2025-11-15 18:48:58 -0300

    feat: complete DTOs implementation

--- src/main/java/onepiece/dto/MissaoDTO.java
+++ src/main/java/onepiece/dto/MissaoDTO.java
@@ -1,4 +1,54 @@
-package org.example.enumerators.src.main.java.onepiece.dto;
+package onepiece.dto;
+
+import onepiece.enums.NivelPerigo;
+import onepiece.enums.StatusMissao;
+import onepiece.enums.TipoMissao;
 
 public class MissaoDTO {
+
+    private Long id;
+    private NivelPerigo nivelPerigo;
+    private TipoMissao tipoMissao;
+    private StatusMissao statusMissao;
+    private Long pirataId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public NivelPerigo getNivelPerigo() {
+        return nivelPerigo;
+    }
+
+    public void setNivelPerigo(NivelPerigo nivelPerigo) {
+        this.nivelPerigo = nivelPerigo;
+    }
+
+    public TipoMissao getTipoMissao() {
+        return tipoMissao;
+    }
+
+    public void setTipoMissao(TipoMissao tipoMissao) {
+        this.tipoMissao = tipoMissao;
+    }
+
+    public StatusMissao getStatusMissao() {
+        return statusMissao;
+    }
+
+    public void setStatusMissao(StatusMissao statusMissao) {
+        this.statusMissao = statusMissao;
+    }
+
+    public Long getPirataId() {
+        return pirataId;
+    }
+
+    public void setPirataId(Long pirataId) {
+        this.pirataId = pirataId;
+    }
 }
--- src/main/java/onepiece/dto/PirataDTO.java
+++ src/main/java/onepiece/dto/PirataDTO.java
@@ -1,4 +1,64 @@
-package org.example.enumerators.src.main.java.onepiece.dto;
+package onepiece.dto;
+
+import onepiece.enums.Raca;
+import onepiece.enums.StatusPirata;
+
+import java.util.List;
 
 public class PirataDTO {
+
+    private Long id;
+    private String nome;
+    private Raca raca;
+    private String tripulacao;
+    private StatusPirata status;
+    private List<MissaoDTO> missoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Raca getRaca() {
+        return raca;
+    }
+
+    public void setRaca(Raca raca) {
+        this.raca = raca;
+    }
+
+    public String getTripulacao() {
+        return tripulacao;
+    }
+
+    public void setTripulacao(String tripulacao) {
+        this.tripulacao = tripulacao;
+    }
+
+    public StatusPirata getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPirata status) {
+        this.status = status;
+    }
+
+    public List<MissaoDTO> getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(List<MissaoDTO> missoes) {
+        this.missoes = missoes;
+    }
 }
